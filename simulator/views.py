159444d1029cd891cded3df7e806c06452409404
Kseraaa <thanapat0918618713@gmail.com>
2024-07-11 11:58:21 +0700

    edit views and setting URLS

--- simulator/views.py
+++ simulator/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Response
+from .serializers import ResponseSerializer
 
-# Create your views here.
+class ResponseViewSet(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
