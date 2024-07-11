8c719a622e2d1bfbef9027b5ee5098268ecaab57
Kseraaa <thanapat0918618713@gmail.com>
2024-07-11 11:56:17 +0700

    create gitignore, model and API for Rest_framwork

--- simulator/models.py
+++ simulator/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Response(models.Model):
+    user_info = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user_info
