ad02206f36b05f6c9de9d1d3ffd4490d33e563b9
Kseraaa <thanapat0918618713@gmail.com>
2024-07-12 00:49:06 +0700

    add url for backend

--- phishing_simulator/urls.py
+++ phishing_simulator/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from simulator.views import phishing_tracker
+from simulator.views import phishing_tracker, get_analysis_data
 
 def index(request):
     return HttpResponse("Welcome to the Phishing Awareness Simulator")
@@ -10,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('simulator.urls')),
     path('phishing_tracker/', phishing_tracker, name='phishing_tracker'),
+    path('api/analysis/', get_analysis_data),
     path('', index),
 ]
