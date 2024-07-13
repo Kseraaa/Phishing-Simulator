1950bedddb49a2a2ed6c11c0a5daf374f15a4f0b
Kseraaa <thanapat0918618713@gmail.com>
2024-07-13 14:25:40 +0700

    update urls file

--- phishing_simulator/urls.py
+++ phishing_simulator/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from simulator.views import phishing_tracker, get_analysis_data
+from simulator.views import phishing_tracker, get_analysis_data, send_phishing_email
 
 def index(request):
     return HttpResponse("Welcome to the Phishing Awareness Simulator")
@@ -9,7 +9,9 @@ def index(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('simulator.urls')),
-    path('phishing_tracker/', phishing_tracker, name='phishing_tracker'),
     path('api/analysis/', get_analysis_data),
+    path('phishing_tracker/', phishing_tracker, name='phishing_tracker'),
+    path('send_phishing_email/', send_phishing_email, name='send_phishing_email'),
+
     path('', index),
 ]
--- simulator/urls.py
+++ simulator/urls.py
@@ -6,8 +6,5 @@ router = DefaultRouter()
 router.register(r'responses', ResponseViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('send_phishing_email/', send_phishing_email, name='send_phishing_email'),
-    path('phishing_tracker/', phishing_tracker, name='phishing_tracker'),
-    path('api/analysis/', get_analysis_data),
 ]
