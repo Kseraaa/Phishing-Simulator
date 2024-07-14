3e17e5b796545faaf6c12edc303c1fefee4df0a8
Kseraaa <thanapat0918618713@gmail.com>
2024-07-14 20:14:12 +0700

    modify web page

--- phishing-frontend/src/App.js
+++ phishing-frontend/src/App.js
@@ -2,26 +2,36 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import PhishingSimulator from './PhishingSimulator';
 import BruteForceAttack from './BruteForceAttack';
+import Ransomeware from './Ransomeware';
+import PasswordTest from './PasswordTest';
+import SocialEngineer from './SocialEngineer';
 import Home from './Home';
+
+import './App.css'; // Import CSS for styling
 
 function App() {
     return (
         <Router>
             <div className="App">
-                
+                <nav className="navbar">
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/phishing-simulator">Phishing</Link></li>
+                        <li><Link to="/brute-force-attack">Brute Force</Link></li>
+                        <li><Link to="/Ransomeware-simulator">Ransomeware</Link></li>
+                        <li><Link to="/SocialEngineer">Social Engineer</Link></li>
+                        <li><Link to="/Password-test">Phishing</Link></li>
+                    </ul>
+                </nav>
+
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/phishing-simulator" component={PhishingSimulator} />
                     <Route path="/brute-force-attack" component={BruteForceAttack} />
+                    <Route path="/Ransomeware-simulator" component={Ransomeware} />
+                    <Route path="/SocialEngineer" component={SocialEngineer} />
+                    <Route path="/Password-test" component={PasswordTest} />
                 </Switch>
-
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/phishing-simulator">Phishing Simulator</Link></li>
-                        <li><Link to="/brute-force-attack">Brute Force Attack</Link></li>
-                    </ul>
-                </nav>
             </div>
         </Router>
     );
---
+++ phishing-frontend/src/Ransomeware.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Ransomeware() {
+    return (
+        <div>
+            <h1>Ransomeware Simulation</h1>
+            <p>Here you can implement your Ransomeware simulation.</p>
+        </div>
+    );
+}
+
+export default Ransomeware;
